add65b466136fe7fd4ce724bf9d0e78ac3c05483
fm-build <fuzeman91+fm-build@gmail.com>
2018-08-27 02:28:40 +1200

    Bumped version to 1.18.8

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ PACKAGE_NAME = 'oem-database-anidb-tmdb-movie'
 def build_config():
     config = {
         'name': PACKAGE_NAME,
-        'version': '1.18.7',
+        'version': '1.18.8',
 
         'author': 'Dean Gardiner',
         'author_email': 'me@dgardiner.net',
